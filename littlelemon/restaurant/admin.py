0cde7869403dee1bc90deabf6553547858a9abd9
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2023-02-26 13:03:26 +0200

    Add files via upload

---
+++ littlelemon/restaurant/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Booking
+from .models import Menu
+
+# Register your models here.
+
+admin.site.register(Menu)
+
